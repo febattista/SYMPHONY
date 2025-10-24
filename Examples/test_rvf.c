34a2f65cfed429f89430b7b6407d91b3747b5a6e
Federico Battista <feb223@lehigh.edu>
2025-10-24 09:05:03 -0400

    Update test_rvf to unit test dual function

--- Examples/test_rvf.c
+++ Examples/test_rvf.c
@@ -6,10 +6,6 @@
 #include <time.h>
 
 #include "symphony.h"
-// feb223
-// These paths should be changed
-// I still don't figure out how to use makefile to 
-// include these files automatically
 #include "../include/sym_types.h"
 #include "../include/sym_master.h"
 
@@ -55,23 +51,14 @@ double *linspace(double a, double b, int pieces){
 
 int main(int argc, char **argv)
 {   
-   
-   int termcode;
-   int numTrain = 10;
-   int numTests = 10;
-   double warmObjVal, coldObjVal, dualFuncObj;
 
-   sym_environment *env_cold = sym_open_environment(); 
    sym_environment *env_warm = sym_open_environment(); 
 
    sym_parse_command_line(env_warm, argc, argv); 
-   sym_parse_command_line(env_cold, argc, argv); 
 
    sym_load_problem(env_warm);
-   sym_load_problem(env_cold);
 
    sym_set_int_param(env_warm, "verbosity", -2);
-   sym_set_int_param(env_cold, "verbosity", -2);
 
    // feb223
    // Those are the parameters to be set in order to 
@@ -92,144 +79,95 @@ int main(int argc, char **argv)
    sym_set_int_param(env_warm, "max_sp_size", 100);
    sym_set_int_param(env_warm, "do_reduced_cost_fixing", FALSE);
    sym_set_int_param(env_warm, "generate_cgl_cuts", FALSE);
-   // sym_set_dbl_param(env_warm, "TM_granularity", 1e-6);
    sym_set_int_param(env_warm, "max_active_nodes", 1);
-   // sym_set_int_param(env_warm, "max_presolve_iter", 0);
-   // sym_set_int_param(env_warm, "limit_strong_branching_time", 0);
 
-   sym_set_int_param(env_cold, "keep_warm_start", TRUE);
-   sym_set_int_param(env_cold, "keep_dual_function_description", TRUE);
-   sym_set_int_param(env_cold, "should_use_rel_br", FALSE);
-   sym_set_int_param(env_cold, "use_hot_starts", FALSE);
-   sym_set_int_param(env_cold, "should_warmstart_node", TRUE);
-   sym_set_int_param(env_cold, "sensitivity_analysis", TRUE);
-   sym_set_int_param(env_cold, "sensitivity_rhs", true);
-   sym_set_int_param(env_cold, "sensitivity_bounds", TRUE);
-   sym_set_int_param(env_cold, "set_obj_upper_lim", FALSE);
-   sym_set_int_param(env_cold, "do_primal_heuristic", FALSE);
-   sym_set_int_param(env_cold, "prep_level", -1);
-   sym_set_int_param(env_cold, "tighten_root_bounds", FALSE);
-   sym_set_int_param(env_cold, "max_sp_size", 100);
-   sym_set_int_param(env_cold, "do_reduced_cost_fixing", FALSE);
-   sym_set_int_param(env_cold, "generate_cgl_cuts", FALSE);
-   sym_set_int_param(env_cold, "max_active_nodes", 1);
-
-   //----------------------
-   // linspace numpy-like 
-   //----------------------
-   // double a = -16, b = 5, zerotol = 1e-7;
-   // int pieces = 2000;
-   // double *zeta_lst = linspace(a, b, pieces);
-   // double *zeta_lst_1 = linspace(-15, 5, 10);
-   // double *rvf_lst  = (double*)malloc(sizeof(double) * pieces);
-   // double *df_lst   = (double*)malloc(sizeof(double) * pieces);
-
+   int i = 0;
+   int num_samples = 10;
    int num_objs = 2;
-   int num_zetas = 30;
-   double zetas[][2] = {
-        {-2747.0, -3132.0}, {-2747.0, -2740.5}, {-2747.0, -2349.0}, {-2747.0, -1957.5}, {-2747.0, -1566.0}, {-2747.0, -1174.5}, {-2747.0, -783.0}, {-2747.0, -391.5}, {-2747.0, -0.0},
-        {-2403.625, -3132.0}, {-2403.625, -2740.5}, {-2403.625, -2349.0}, {-2403.625, -1957.5}, {-2403.625, -1566.0}, {-2403.625, -1174.5}, {-2403.625, -783.0}, {-2403.625, -391.5}, {-2403.625, -0.0},
-        {-2060.25, -3132.0}, {-2060.25, -2740.5}, {-2060.25, -2349.0}, {-2060.25, -1957.5}, {-2060.25, -1566.0}, {-2060.25, -1174.5}, {-2060.25, -783.0}, {-2060.25, -391.5}, {-2060.25, -0.0},
-        {-1716.875, -3132.0}, {-1716.875, -2740.5}, {-1716.875, -2349.0}, {-1716.875, -1957.5}, {-1716.875, -1566.0}, {-1716.875, -1174.5}, {-1716.875, -783.0}, {-1716.875, -391.5}, {-1716.875, -0.0},
-        {-1373.5, -3132.0}, {-1373.5, -2740.5}, {-1373.5, -2349.0}, {-1373.5, -1957.5}, {-1373.5, -1566.0}, {-1373.5, -1174.5}, {-1373.5, -783.0}, {-1373.5, -391.5}, {-1373.5, -0.0},
-        {-1030.125, -3132.0}, {-1030.125, -2740.5}, {-1030.125, -2349.0}, {-1030.125, -1957.5}, {-1030.125, -1566.0}, {-1030.125, -1174.5}, {-1030.125, -783.0}, {-1030.125, -391.5}, {-1030.125, -0.0},
-        {-686.75, -3132.0}, {-686.75, -2740.5}, {-686.75, -2349.0}, {-686.75, -1957.5}, {-686.75, -1566.0}, {-686.75, -1174.5}, {-686.75, -783.0}, {-686.75, -391.5}, {-686.75, -0.0},
-        {-343.375, -3132.0}, {-343.375, -2740.5}, {-343.375, -2349.0}, {-343.375, -1957.5}, {-343.375, -1566.0}, {-343.375, -1174.5}, {-343.375, -783.0}, {-343.375, -391.5}, {-343.375, -0.0},
-        {0.0, -3132.0}, {0.0, -2740.5}, {0.0, -2349.0}, {0.0, -1957.5}, {0.0, -1566.0}, {0.0, -1174.5}, {0.0, -783.0}, {0.0, -391.5}, {0.0, -0.0}
-    };
-   // double zetas[][2] = {{-7439.0, -7228.0}, {-7439.0, -6324.5}, {-7439.0, -5421.0}, {-7439.0, -4517.5}, {-7439.0, -3614.0}, 
-   //                      {-7439.0, -2710.5}, {-7439.0, -1807.0}, {-7439.0, -903.5}, {-7439.0, -0.0}, {-6509.125, -7228.0}, 
-   //                      {-6509.125, -6324.5}, {-6509.125, -5421.0}, {-6509.125, -4517.5}};
-      
+   int termcode;
+   double dualFuncObj = 0;
+   double warmObjFunc = 0;
+   double infinity = 1e10;
 
-
-   // double zetas[][2] = {{-12939.0, -13077.0}, {-12939.0, -11442.375}, {-12939.0, -9807.75}, {-12939.0, -8173.125}, 
-   //                      {-12939.0, -6538.5}, {-12939.0, -4903.875}, {-12939.0, -3269.25}, {-12939.0, -1634.625}, 
-   //                      {-12939.0, -0.0}, {-11321.625, -13077.0}, {-11321.625, -11442.375}, {-11321.625, -9807.75}, 
-   //                      {-11321.625, -8173.125}};
-   double rhs[2] = {0, 0};
-   
-   // First solve
-   char filename[33] = "kp_test"; 
-   sym_write_lp(env_warm, filename);
-   if ((termcode = sym_solve(env_warm)) < 0){
-      printf("WARM: PROBLEM INFEASIBLE!\n");
-   }
-   sym_build_dual_func(env_warm);
-   print_dual_function(env_warm);
-   sym_evaluate_dual_function(env_warm, rhs, 0, &dualFuncObj);
-   print_dual_function(env_warm);
-
-   if (sym_is_proven_optimal(env_warm)){
-      sym_get_obj_val(env_warm, &warmObjVal);
-      printf(" DF: %.10f\n", dualFuncObj);
-      printf("RVF: %.10f\n", warmObjVal);
-      assert((fabs(dualFuncObj - warmObjVal) < 1e-5));
-   } else if (sym_is_proven_primal_infeasible(env_warm)) {
-      assert(dualFuncObj > 1e10);
-      printf("RVF: INFEASIBLE\n");
-      printf(" DF: %.10f\n", dualFuncObj);
+   double **rhss = (double **)malloc(sizeof(double*) * num_objs);
+   double *objVals = (double *)malloc(sizeof(double) * num_samples);
+   for (i = 0; i < num_objs; i++){
+      rhss[i] = linspace(0, 3000, num_samples);
    }
 
-   printf("======================================\n");
+   /************************************************************************\
+    * Assert the dual function is strong at the tested RHSs
+   \************************************************************************/
+   for (i = 0; i < num_samples; i++){
+      double *rhs = (double *)malloc(sizeof(double) * num_objs);
+      rhs[0] = rhss[0][i];
+      rhs[1] = rhss[1][i];
 
-   for (int i = 0; i < num_zetas; i++){
-      rhs[0] = zetas[i][0];
-      rhs[1] = zetas[i][1];
-      printf("======================================\n");
-      printf(" RHS: (%.2f, %.2f)\n", rhs[0], rhs[1]);
-      printf("======================================\n");
-      
       set_rhs(env_warm, rhs, num_objs);
-    
+      
+      
       if ((termcode = sym_warm_solve(env_warm)) < 0){
          printf("WARM: PROBLEM INFEASIBLE!\n");
       }
 
       if (sym_is_proven_optimal(env_warm)){
-         printf("  PROVEN OPTIMAL!\n");
-         sym_get_obj_val(env_warm, &warmObjVal);
-         printf("RVF WARM: %.10f\n", warmObjVal);
+         
       } else if (sym_is_proven_primal_infeasible(env_warm)){
-         printf("  PROVEN INFEASIBLE!\n");
+   
       }
 
+      sym_get_obj_val(env_warm, &warmObjFunc);
       sym_build_dual_func(env_warm);
-      print_dual_function(env_warm);
-      sym_evaluate_dual_function(env_warm, rhs, 2, &dualFuncObj);
-      print_dual_function(env_warm);
+      sym_evaluate_dual_function(env_warm, rhs, num_objs, &dualFuncObj);
 
-      set_rhs(env_cold, rhs, num_objs);
-      if ((termcode = sym_solve(env_cold)) < 0){
-         printf("WARM: PROBLEM INFEASIBLE!\n");
+      if (sym_is_proven_optimal(env_warm)){
+         printf("RHS: [%.2f, %.2f]  -->  PROVEN OPTIMAL!\n", 
+            rhs[0], rhs[1]);
+         printf(" DF: %.10f  RVF: %.10f\n", dualFuncObj, warmObjFunc);
+         objVals[i] = warmObjFunc;
+         assert(fabs(dualFuncObj - warmObjFunc) < 1e-5); 
+      } else if (sym_is_proven_primal_infeasible(env_warm)){
+         printf("RHS: [%.2f, %.2f]  -->  PROVEN INFEASIBLE!\n", rhs[0], rhs[1]);
+         printf(" DF: %.10f\n", dualFuncObj, warmObjFunc);
+         objVals[i] = infinity;
+         assert(dualFuncObj > infinity);
       }
-     
-      if (sym_is_proven_optimal(env_cold)){
-         sym_get_obj_val(env_cold, &warmObjVal);
-         printf(" DF: %.10f\n", dualFuncObj);
-         printf("RVF: %.10f\n", warmObjVal);
-         if (fabs(dualFuncObj - warmObjVal) >= 1e-5){
-            print_dual_function(env_warm);
-            sym_evaluate_dual_function(env_warm, rhs, 2, &dualFuncObj);
-            print_dual_function(env_warm);
-         }
-         assert((fabs(dualFuncObj - warmObjVal) < 1e-5));
-      } else if (sym_is_proven_primal_infeasible(env_cold)) {
-         assert(dualFuncObj > 1e10);
-         printf("RVF: INFEASIBLE\n");
-         printf(" DF: %.10f\n", dualFuncObj);
-      }
+
+      free(rhs);
    }
 
-   printf("LP TIME from DUAL FUNC: %.5f\n", sym_get_lp_time_dual_func(env_warm));
+   print_dual_function(env_warm);
+
+   /************************************************************************\
+    * Assert the dual function remains strong at tested RHSs
+   \************************************************************************/
+   for (i = 0; i < num_samples; i++){
+      double *rhs = (double *)malloc(sizeof(double) * num_objs);
+      rhs[0] = rhss[0][i];
+      rhs[1] = rhss[1][i];
+
+      sym_evaluate_dual_function(env_warm, rhs, num_objs, &dualFuncObj);
+      if (objVals[i] > infinity/10){
+         assert(dualFuncObj > infinity);
+         printf("RHS: [%.2f, %.2f]  -->  PROVEN INFEASIBLE!\n", rhs[0], rhs[1]);
+         printf(" DF: %.10f\n", dualFuncObj);
+      } else {
+         printf("RHS: [%.2f, %.2f]  -->  PROVEN OPTIMAL!\n", 
+            rhs[0], rhs[1]);
+         printf(" DF: %.10f  RVF: %.10f\n", dualFuncObj, objVals[i]);
+         assert(fabs(dualFuncObj - objVals[i]) < 1e-5); 
+      }
+     
+      free(rhs);
+   }
 
    // Memory clean-ups
    sym_close_environment(env_warm);
-   sym_close_environment(env_cold);
-
-   // free(zeta_lst);
-   // free(rvf_lst);
-   // free(df_lst);
+   free(objVals);
+   for (i = 0; i < num_objs; i++){
+      free(rhss[i]);
+   }
+   free(rhss);
 
    return 0;
 }  
